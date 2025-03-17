50ab7c6b6e40dd208288360721766314ada08c98
rdz <l22310532@lapaz.tecnm.mx>
2025-03-17 16:57:45 -0700

    referencias add

---
+++ Metodos Numeros/Metodos Numeros/NumericMethods.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NCalc;
+using LiveCharts;
+using System.Windows.Forms;
+using NCalc.Domain;
+
+namespace Metodos_Numeros
+{
+    public static class NumericMethods
+    {
+        public static void Biseccion(string funcion, int a, int b, float error, DataGridView dataGridView)
+        {
+            
+        }
+        public static void ReglaFalsa(string funcion, int a, int b, float error, DataGridView dataGridView)
+        {
+
+        }
+        public static void Grafica(string funcion, int a, int b)
+        {
+
+        }
+    }
+}
